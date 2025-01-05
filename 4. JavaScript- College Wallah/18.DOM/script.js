e9e9b715a4ca6bd0cfbc9b9ae42290267cf5cfb9
tanaya2005 <tanayaitis@gmail.com>
2025-01-05 11:25:26 +0530

    .

---
+++ 4. JavaScript- College Wallah/18.DOM/script.js
@@ -0,0 +1,8 @@
+let a=document.querySelector("h1"); //semicolons imp yahape
+// console.log(x);
+a.style.color="red";
+a.style.fontFamily="Gill Sans"
+
+let b=document.querySelector("h2");
+b.style.backgroundColor="yellow";
+b.innerHTML="changed"; //can change html ka code this waya
---
+++ 4. JavaScript- College Wallah/18.DOM/script2.js
@@ -0,0 +1,14 @@
+let x=document.querySelector("h3");
+setTimeout(function(){
+    x.style.backgroundColor="aqua";
+},2000);
+
+let y=document.querySelector("h4");
+setTimeout(function(){
+    y.style.color="purple";
+},3500);
+
+setTimeout(function(){
+    y.innerHTML="Letsgoooooooooooo"
+    y.style.fontFamily="arial";
+}, 4500);
