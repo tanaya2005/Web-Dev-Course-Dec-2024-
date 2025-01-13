d4d18b652791d218559bf6ed4b0371402b32d389
tanaya2005 <tanayaitis@gmail.com>
2025-01-13 15:21:38 +0530

    form handling done. have to complete components also today.

--- 4. React- Sheriyans Coding/1st-vite-project/src/App.jsx
+++ 4. React- Sheriyans Coding/1st-vite-project/src/App.jsx
@@ -77,13 +77,123 @@
 // export default App
 
 
+
 //4th code--------------------------------------------------
-import React from 'react'
+//writing tailwaind css and html code in react app.jsx
+
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1 className='text-5xl bg-pink-600 text-white'>Hello guys </h1>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+//5th code--------------------------------------------------
+//form handling in reactjs and preventing the default behaviour of the form that is reloading the page everytime we submit the form
+
+// import React, { useState } from 'react'
+
+// const App = () => {
+
+
+//   const Submithandlder =(e)=>{
+//     e.preventDefault(); //func to prevent the default behaviour of the form that is reloading the page everytime we submit the form
+//     console.log("Form submitted")
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={(e)=>{
+//         Submithandlder(e);
+//       }}>
+//         <input 
+//         className='px-4 py-2 m-5' type="text" placeholder='Enter your name' name='name'/>
+//         <button className='px-3'> Submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+//6th code--------------------------------------------------
+//form handling and displaying the entered value in the form with the help of useState and asking react to make changes in the DOM along with the form getting blank after we type name and click on submit
+
+
+// import React, { useState } from 'react'
+
+// const App = () => {
+
+
+//   const Submithandlder =(e)=>{
+//     e.preventDefault();
+//     console.log(USername);
+//     setUSername(''); //inout goes blank after submitting the form
+//   }
+
+//   const [USername, setUSername ] = useState('')
+
+
+//   return (
+//     <div>
+//       <form onSubmit={(e)=>{
+//         Submithandlder(e);
+//       }}>
+//         <input 
+//         value={USername} //this is k/as 2 way binding
+//         onChange={(e)=> setUSername(e.target.value)}
+//         className='px-4 py-2 m-5' type="text" placeholder='Enter your name' name='name'/>
+//         <button className='px-3'> Submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+//------------------------------------------
+//practicing 6th code again adding age alsooooooo ayayyayayayy
+
+import React, { useState } from 'react'
 
 const App = () => {
+  
+  const Submithandlder=(e)=>{
+    e.preventDefault();
+    console.log("Form submitted");
+    setusername('');
+    setage('');
+  }
+
+  const [username, setusername] = useState('')
+  const [age, setage] = useState('')
+
+
   return (
     <div>
-      <h1 className='text-5xl bg-pink-700 text-black'>Hello guys </h1>
+      <form onSubmit={(e)=>{
+        Submithandlder(e);
+      }}>
+        <input 
+        value={username}
+        onChange={(e)=> setusername(e.target.value)} className='m-5 px-4 py-3 rounded border'
+        placeholder='Enter your name' type="text" name="username" id="username" />
+        <input 
+        value={age}
+        onChange={(e)=> setage(e.target.value)} className='m-5 px-4 py-3 rounded border'
+        placeholder='Enter your age' type="number" name="age" id="age" />
+        <button className='rounded p-3 m-3'>Submit </button>
+      </form>
     </div>
   )
 }
