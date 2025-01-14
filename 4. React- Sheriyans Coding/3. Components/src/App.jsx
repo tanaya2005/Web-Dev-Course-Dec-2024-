1e5c5213a1f1b5864d1b234ee89b1d750b686078
tanaya2005 <tanayaitis@gmail.com>
2025-01-14 23:12:36 +0530

    .

---
+++ 4. React- Sheriyans Coding/3. Components/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <nav>
+      <h1>
+        Welcome to your account!
+      </h1>
+      <div>
+        <h4>About</h4>
+        <h4>Contact</h4>
+        <h4></h4>
+        <h4></h4>
+      </div>
+    </nav>
+    </>
+  )
+}
+
+export default App
